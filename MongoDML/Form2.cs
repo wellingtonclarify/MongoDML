60cadae45a9bc36e738a2be21c11f04f852d6ae1
Wellington Alves dos Santos <wellington.santos@clarifysistemas.com.br>
2020-07-07 11:07:31 -0300

    .

--- MongoDML/Form2.cs
+++ MongoDML/Form2.cs
@@ -73,11 +73,11 @@ namespace MongoDML
         private void xComboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
             var cerc = (Cerca)xComboBox2.SelectedItem;
-            if (cerc.Tipo == "GARAGEM")
-            {
+            //if (cerc.Tipo == "GARAGEM")
+            //{
                 xTextBox2.Text = cerc.LatitudeGaragem?.ToString();
                 xTextBox3.Text = cerc.LongitudeGaragem?.ToString();
-            }
+            //}
             RefreshPreview();
         }
 
